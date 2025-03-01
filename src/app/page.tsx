49278d0e07792d5df91d86805a76302064c1e234
hodisanmihai <mihaihodisn@gmail.com>
2025-03-01 16:50:41 +0200

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import Footer from "./components/Footer";
 
 export default function Home() {
   return (
-    <div className="overflow-x-hidden">
+    <div className="overflow-x-hidden no-copy">
       <Navbar />
       <Hero />
       <About />
